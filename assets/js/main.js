33960bde4569c86ccacc6895caa9650f22176652
Sum Duong <sumsvpro@gmail.com>
2021-07-06 15:10:08 +0700

    animation at header when scroll v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var lastScrollTop = 0;
+const headerInfor = document.querySelector('.header__infor');
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+document.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop){
+        // downscroll code
+        console.log("down");
+        if(window.pageYOffset>50 && !headerInfor.classList.contains('sticky')) {
+            headerInfor.classList.add('sticky');
+        }
+    } else {
+        // upscroll code
+        console.log("up");
+        if(window.pageYOffset<50 && headerInfor.classList.contains('sticky')) {
+            headerInfor.classList.remove('sticky');
+        }
+    }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
