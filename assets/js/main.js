5c6775b2185bbdf585eed09b48576f1f5e8ccfdd
Sum Duong <sumsvpro@gmail.com>
2021-07-06 16:11:39 +0700

    create link list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var lastScrollTop = 0;
 const headerInfor = document.querySelector('.header__infor');
+const linkList = document.querySelector('.link-list');
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 document.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
@@ -8,6 +9,7 @@ document.addEventListener("scroll", function(){ // or window.addEventListener("s
         console.log("down");
         if(window.pageYOffset>50 && !headerInfor.classList.contains('sticky')) {
             headerInfor.classList.add('sticky');
+            // link-list.style.margin-top = 
         }
     } else {
         // upscroll code
